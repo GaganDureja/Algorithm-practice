cd7428e58dcea0af1e4432f718962270f7f6446d
Gagan <gagandureja675@gmail.com>
2020-10-15 19:53:48 +0530

    Date 15 Oct

---
+++ super reduced.py
@@ -0,0 +1,22 @@
+#Link:  https://edabit.com/challenge/JBTLDxvdzpjyJA4Nv
+
+
+
+
+
+def super_reduced_string(txt):	
+	for x in range(len(txt)-1):		
+		if txt[x]==txt[x+1]:			
+			return super_reduced_string(txt[:x]+txt[x+2:])	
+	return txt if txt else 'Empty String'
+
+
+
+	
+
+
+
+
+print(super_reduced_string("aaabccddd"))
+print(super_reduced_string('abbccddfghiaklmno'))
+print(super_reduced_string('bccddb'))
