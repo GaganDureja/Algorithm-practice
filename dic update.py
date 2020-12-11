c6db81a5af9921b739530d4a78a336835b7c99c1
Gagan <gagandureja675@gmail.com>
2020-12-12 00:21:12 +0530

    Daily coding problem

---
+++ dic update.py
@@ -0,0 +1,15 @@
+Link:  https://edabit.com/challenge/owmKG47zYRJw72aiD
+
+
+
+
+
+
+
+def add_name(obj, name, value):
+	obj.update({name:value})
+	return obj
+
+
+print(add_name({}, "Brutus", 300))
+print(add_name({'piano': 500}, "Brutus", 400))
