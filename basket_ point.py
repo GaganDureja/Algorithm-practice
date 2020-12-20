37a2f4a95df970948fbf7afb5501b186d249b202
Gagan <gagandureja675@gmail.com>
2020-12-20 23:51:00 +0530

    Daily coding problem

---
+++ basket_ point.py
@@ -0,0 +1,22 @@
+# Link:  https://edabit.com/challenge/KWnJrMzK9CumnfxTF
+
+
+
+
+
+
+
+
+
+
+
+def points(twopointers, threepointers):
+	return twopointers*2 + threepointers*3
+
+
+
+print(points(1,1))
+print(points(7,5))
+print(points(38,8))
+
+
