959cf8d93e02a911411f9ecffc8e6a3e9c74cfcf
Gagan <gagandureja675@gmail.com>
2020-12-21 23:11:22 +0530

    Weekly coding problem

---
+++ check_empty.py
@@ -0,0 +1,18 @@
+Link: https://edabit.com/challenge/wtu32ZFxHJsuQnogX
+
+
+
+
+def is_empty(s):
+	return not s
+
+
+
+
+
+print(is_empty(""))
+print(is_empty(" "))
+print(is_empty("            "))
+print(is_empty("38215"))
+print(is_empty("afjabsdf"))
+print(is_empty("!?@&"))
