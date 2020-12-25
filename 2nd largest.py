6f7f11e73aaa80d6e4c9c69b22d3464888933308
Gagan <gagandureja675@gmail.com>
2020-12-25 22:56:55 +0530

    Weekly codig problem

---
+++ 2nd largest.py
@@ -0,0 +1,19 @@
+# Link:  https://edabit.com/challenge/ZMW4xHjMNf62snrkL
+
+
+
+
+
+
+
+
+
+def second_largest(lst):
+	return sorted(lst)[-2]
+
+
+
+
+print(second_largest([10, 40, 30, 20, 50]))
+print(second_largest([513, 211, 131, 417, 11]))
+print(second_largest([63, 44, 11, 22, 33, 66, 65]))
