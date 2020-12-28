807f5c8717257b152d6abcec2117476214268383
Gagan <gagandureja675@gmail.com>
2020-12-28 23:57:06 +0530

    Daily coding problem

---
+++ future people.py
@@ -0,0 +1,21 @@
+#Link:  https://edabit.com/challenge/dMEAuGWoud3QjBwwx
+
+
+
+
+
+
+
+
+
+def future_people(P, nP):
+	return P+(360*nP)
+
+
+
+
+
+
+print(future_people(256,2))
+print(future_people(3248,6))
+print(future_people(5240,3))
