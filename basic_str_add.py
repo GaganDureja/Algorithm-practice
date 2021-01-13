4875540f580d6fd133e420104144bb0f5ef53025
GaganDureja <gagandureja675@gmail.com>
2021-01-13 21:30:11 +0530

    Daily coding problem

---
+++ basic_str_add.py
@@ -0,0 +1,22 @@
+# Link:  https://edabit.com/challenge/YN9mNNc4mMPDxyhFf
+
+
+
+
+
+
+
+
+
+
+
+def name_string(name):
+	  return name+'Edabit'
+
+
+
+
+
+print(name_string('Mubashir'))
+print(name_string('Matt'))
+print(name_string('python'))
