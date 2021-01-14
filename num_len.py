4068fcdbf9c9a5f17cd010dd4c00adc64db57739
GaganDureja <gagandureja675@gmail.com>
2021-01-14 22:17:41 +0530

    Daily coding problem

---
+++ num_len.py
@@ -0,0 +1,22 @@
+#Link:  https://edabit.com/challenge/2zKetgAJp4WRFXiDT
+
+
+
+
+
+
+
+
+
+
+
+def number_length(num):
+ return sum(1 for x in str(num))
+ 
+
+
+
+
+print(number_length(10))
+print(number_length(5000))
+print(number_length(0))
