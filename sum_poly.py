060a9a4b8c2fd9e21ba068b6c509810a400ebe3c
GaganDureja <gagandureja675@gmail.com>
2021-01-22 14:21:22 +0530

    Daily coding problem

---
+++ sum_poly.py
@@ -0,0 +1,26 @@
+# Link:  https://edabit.com/challenge/mDzheHpwtqyXePEBE
+
+
+
+
+
+
+
+
+
+def sum_polygon(n):
+	return (n-2)*180
+
+	
+
+
+
+
+print(sum_polygon(3))
+print(sum_polygon(4))
+print(sum_polygon(6))
+
+
+
+
+
