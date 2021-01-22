88d789823ae1f79a78ab1921b3c6b5e259e93b9a
GaganDureja <gagandureja675@gmail.com>
2021-01-22 14:25:20 +0530

    Daily coding problem

---
+++ ret_negative.py
@@ -0,0 +1,20 @@
+#Link: https://edabit.com/challenge/j9hv6sXmfvqkicLJC
+
+
+
+
+
+
+
+
+def return_negative(n):
+	return n if n<0 else -n
+
+
+
+print(return_negative(5))
+print(return_negative(-15))
+print(return_negative(0))
+
+
+
