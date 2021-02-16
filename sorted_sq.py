615ad0c887a9b8a98743b8ceacf3b8620d72ee40
GaganDureja <gagandureja675@gmail.com>
2021-02-16 22:49:48 +0530

    Daily coding problem: 118

---
+++ sorted_sq.py
@@ -0,0 +1,26 @@
+# This problem was asked by Google.
+
+# Given a sorted list of integers, square the elements and give the output in sorted order.
+
+# For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def sortsq(lst):
+	return sorted([x*x for x in lst])
+
+
+
+print(sortsq([-9,-2,0,2,3]))
