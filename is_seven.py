28ba0f986edf7f62c6f7e32463479cb816bc8fbd
GaganDureja <gagandureja675@gmail.com>
2021-02-24 00:24:34 +0530

    Daily coding problem

---
+++ is_seven.py
@@ -0,0 +1,32 @@
+# https://edabit.com/challenge/iBL3eDRWzpxgfQyHx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def is_seven(x):
+	return x==7
+
+
+
+
+
+
+
+
+print(is_seven(4))
+print(is_seven(7))
+
+print(is_seven(10))
+
+print(is_seven(77))
