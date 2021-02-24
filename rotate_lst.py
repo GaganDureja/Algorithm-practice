7da993d352e54372634fb3752a9c363979f04544
GaganDureja <gagandureja675@gmail.com>
2021-02-24 22:42:54 +0530

    Daily coding Problem: 126

---
+++ rotate_lst.py
@@ -0,0 +1,32 @@
+# This problem was asked by Facebook.
+
+# Write a function that rotates a list by k elements. For example, [1, 2, 3, 4, 5, 6]
+# rotated by two becomes [3, 4, 5, 6, 1, 2]. Try solving this without creating a copy
+# of the list. How many swap or move operations do you need?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def rotate_lst(lst,k):
+	k%=len(lst)
+	return lst[k:]+lst[:k]
+
+	
+
+
+
+print(rotate_lst([1,2,3,4,5,6],2))
+print(rotate_lst([1,2,3,4,5,6],6))
+print(rotate_lst([1,2,3,4,5,6],9))
