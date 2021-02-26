deaf25d2fc906227d60f2e99228ce21b7477aac5
GaganDureja <gagandureja675@gmail.com>
2021-02-26 21:52:12 +0530

    Daily coding problem

---
+++ bool_to_str.py
@@ -0,0 +1,25 @@
+#Link:  https://edabit.com/challenge/QQp2o22huzBCkHesy
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bool_to_string(flag):
+	return str(flag)
+
+
+
+
+
+
+
+print(bool_to_string(True))
+print(bool_to_string(False))
