b7a24035a10df057e91154b1e253afa5a5eec581
GaganDureja <gagandureja675@gmail.com>
2021-02-28 22:16:07 +0530

    Daily coding problem: 129

---
+++ sq_root.py
@@ -0,0 +1,27 @@
+#Given a real number n, find the square root of n. For example, given n = 9, return 3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+import math
+
+def sq_root(n):
+	return math.sqrt(n)
+
+	
+
+
+
+print(sq_root(9))
+print(sq_root(49))
+print(sq_root(10))
+
