2b67e37243799378e8b9caac1b88a52fbb30e49f
GaganDureja <gagandureja675@gmail.com>
2021-03-01 22:16:42 +0530

    Daily coding problem

---
+++ add_edabit.py
@@ -0,0 +1,28 @@
+#Link:   https://edabit.com/challenge/YN9mNNc4mMPDxyhFf
+
+
+
+
+
+
+
+
+
+
+
+def name_string(name):
+	return name + 'Edabit'
+
+
+
+
+
+
+
+
+
+
+
+print(name_string('Mubashir'))
+print(name_string('Matt'))
+print(name_string('python'))
