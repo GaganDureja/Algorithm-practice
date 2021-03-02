664859aace3dfe1fb97f8155a567bb154276610e
GaganDureja <gagandureja675@gmail.com>
2021-03-02 23:17:42 +0530

    Daily coding problem

---
+++ points_count.py
@@ -0,0 +1,28 @@
+# https://edabit.com/challenge/KWnJrMzK9CumnfxTF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def points(twopointers, threepointers):
+	return twopointers*2 + threepointers*3
+
+
+
+
+
+
+
+print(points(2,2))
+print(points(2,1))
+print(points(69,204))
