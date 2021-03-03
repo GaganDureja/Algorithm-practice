d400f51f63551e73f852a7c7380008344df1c03a
GaganDureja <gagandureja675@gmail.com>
2021-03-03 21:58:13 +0530

    Date 03 Mar

---
+++ tri_area.py
@@ -0,0 +1,27 @@
+# Link:  https://edabit.com/challenge/3CaszbdZYGN4otQD8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def triArea(base, height):
+	return base*height /2
+
+
+
+
+
+print(triArea(3, 2))
+print(triArea(5, 4))
+print(triArea(10, 10))
