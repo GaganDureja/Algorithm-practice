8d70f134ec0684a855ad56da9dcc011d4fda2517
GaganDureja <gagandureja675@gmail.com>
2021-05-23 00:01:45 +0530

    Edabit easy

---
+++ nth_even_num.py
@@ -0,0 +1,21 @@
+#link:  https://edabit.com/challenge/WYq4aFwSNuoyFCW5G
+
+
+
+
+
+
+
+def nth_even(n):
+	return(2*n-2)
+
+
+
+
+
+
+
+print(nth_even(1))
+print(nth_even(2))
+print(nth_even(3))
+print(nth_even(100))
